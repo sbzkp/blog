1cd3654d65286546fa2f123291da2ea775eb9d83
果果 <guoguo@aaaa.local>
2019-06-12 15:16:07 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,28 @@
+#### 项目说明              
+* 该项目使用 [Create React App](https://github.com/facebookincubator/create-react-app)脚手架创建的
+
+#### 如何启动
+* start
+```
+npm start
+```
+ or
+```
+yarn start
+```
+* build
+```
+npm run build
+```
+ or
+```
+yarn build
+```
+
+#### 项目框架
+
+该项目使用的react+react-router+mobx+antd全家桶               
+
+#### 项目后台
+
+待陈集武启动开发
---
+++ src/routes/blog/index.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import 'styles/motto.scss'
+import './style.scss'
+
+class Blog extends Component {
+  state = {
+  
+  }
+  render() {
+    return (
+      <div className="blog">
+        <div className="content">
+          <div className="articel">1</div>
+          <div className="extra">1</div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Blog
